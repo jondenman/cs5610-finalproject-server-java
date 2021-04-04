8909b2e3579569d08d9015895e32f8bff4ca8598
denmanj <denman.j@northeastern.edu>
2021-04-04 13:17:14 -0400

    implemented rest of User CRUD operations and tested with postman

--- src/main/java/com/example/cs5610finalprojectserverjava/repositories/UserRepository.java
+++ src/main/java/com/example/cs5610finalprojectserverjava/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.example.cs5610finalprojectserverjava.repositories;
 
 import com.example.cs5610finalprojectserverjava.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+
+
 }
